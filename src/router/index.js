7f4cbb3ae04a2684eb8a35d9380c2d49e8a5fc22
baltierra <fabs@illinois.edu>
2026-07-30 09:19:39 -0500

    chore: align route name with path and remove stale scaffold comments

--- src/router/index.js
+++ src/router/index.js
@@ -14,11 +14,7 @@ const router = createRouter({
     },
     {
       path: "/about",
-      // name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      name: "acknowledgements",
+      name: "about",
       meta: { title: "Acknowledgements" },
       component: () => import("../views/AboutView.vue"),
     },
